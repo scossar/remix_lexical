7779f3e46801ba6468a1176c0307ba1397292056
scossar <scossar3@gmail.com>
2024-04-29 21:10:25 -0700

    set active states for buttons

--- app/components/CustomTextActions.tsx
+++ app/components/CustomTextActions.tsx
@@ -1,39 +1,53 @@
+import { useState } from "react";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { FORMAT_TEXT_COMMAND, TextFormatType } from "lexical";
 
 export const CustomTextActions = () => {
   const [editor] = useLexicalComposerContext();
+  const [activeFormats, setActiveFormats] = useState<Record<string, boolean>>({
+    bold: false,
+    italic: false,
+    code: false,
+    highlight: false,
+    strikethrough: false,
+  });
 
   const handleOnClick = (formatType: TextFormatType) => {
+    const isActive = !activeFormats[formatType];
+    console.log(
+      `handling click for formatType: ${formatType}. isActive: ${isActive}`
+    );
+    console.log(`format obj: ${JSON.stringify(activeFormats, null, 2)}`);
     editor.dispatchCommand(FORMAT_TEXT_COMMAND, formatType);
+    setActiveFormats((prev) => ({ ...prev, [formatType]: isActive }));
   };
 
   return (
     <div style={{ marginTop: "10px" }}>
       <span style={{ fontWeight: "bold" }}>Text actions</span>
       <div>
-        {[
-          "Bold",
-          "Italic",
-          "Underline",
-          "Code",
-          "Highlight",
-          "Strikethrough",
-          "Subscript",
-          "Superscript",
-        ].map((value, index) => {
-          return (
-            <button
-              className="px-2 py-1 mr-1 border rounded-sm border-slate-499"
-              key={index}
-              onClick={() =>
-                handleOnClick(value.toLowerCase() as TextFormatType)
-              }
-            >
-              {value}
-            </button>
-          );
-        })}
+        {["Bold", "Italic", "Code", "Highlight", "Strikethrough"].map(
+          (formatType, index) => {
+            console.log(
+              `in button for formatType: ${formatType}, active? ${activeFormats[formatType]}`
+            );
+            return (
+              <button
+                className={`px-2 py-1 mr-1 border rounded-sm border-slate-500 ${
+                  activeFormats[formatType.toLowerCase()]
+                    ? "bg-blue-500 text-white"
+                    : "bg-white text-slate-500"
+                }`}
+                key={index}
+                onClick={() =>
+                  handleOnClick(formatType.toLowerCase() as TextFormatType)
+                }
+              >
+                {formatType}
+              </button>
+            );
+          }
+        )}
       </div>
     </div>
   );
